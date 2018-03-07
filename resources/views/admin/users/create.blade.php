03cd5c841d13500de04b8098a9c4962de02e1051
Matej Đuroković <matejdurokovic@gmail.com>
2018-03-07 20:10:45 +0100

    photos upload and creating users working

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -28,8 +28,8 @@
 		{!! Form::select('is_active', [1=>'Active', 0=>'NOT Active'], 0, ['class'=>'form-control']) !!}
 	</div>
 	<div class="form-group">
-		{!! Form::label('file', 'File:') !!}
-		{!! Form::file('file', null, ['class'=>'form-control']) !!}
+		{!! Form::label('photo_id', 'Photo:') !!}
+		{!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
 	</div>
 	
 	
